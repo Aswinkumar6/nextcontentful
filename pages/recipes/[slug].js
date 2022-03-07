bec04a8bdfaa0146f1ac3c8c37c0dac4acbf0b37
aswinkumar <aswinkumar86@hotmail.com>
2022-03-07 22:51:25 +0530

    initial

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,92 @@
-export default function RecipeDetails() {
+
+import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+const client = createClient({
+      space: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    });
+
+    export const getStaticPaths = async()=>{
+        const res = await client.getEntries({ content_type: "recipe" });
+
+        const paths =res.items.map(item=>{
+            return {
+                params:{slug:item.fields.slug}
+            }
+        })
+        return {
+          paths: paths,fallback:false
+          }
+    }
+export async function getStaticProps({params}) {
+  
+  const {items} = await client.getEntries({ content_type: "recipe",
+'fields.slug':params.slug });
+  return {
+    props: {
+      recipe: items[0],revalidate:10
+    },
+  };
+}
+
+export default function RecipeDetails({recipe}) {
+    console.log('recipe: ', recipe);
+        const { featuredImage, title, cookingTime, ingredients,methods } = recipe.fields;
+        console.log('ingredients: ', ingredients);
+        console.log('title: ', title);
+        console.log('featuredImage: ', featuredImage);
+
   return (
     <div>
-      Recipe Details
+      <div className="banner">
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{title}</h2>
+      </div>
+      <div className="info">
+        <p>Takes approx {cookingTime} mins to make</p>
+        <h3>Ingredients:</h3>
+        {ingredients.map((ing) => {
+          console.log("ing: ", ing);
+          return <span key={ing}>{ing}</span>;
+        })}
+      </div>
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(methods)}</div>
+      </div>
+      <style jsx>
+        {`
+          h2,
+          h3 {
+            text-transform: uppercase;
+          }
+          .banner h2 {
+            margin: 0;
+            background: #fff;
+            display: inline-block;
+            padding: 20px;
+            position: relative;
+            top: -60px;
+            left: -10px;
+            transform: rotateZ(-1deg);
+            box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          }
+          .info p {
+            margin: 0;
+          }
+          .info span::after {
+            content: ", ";
+          }
+          .info span:last-child::after {
+            content: ".";
+          }
+        `}
+      </style>
     </div>
-  )
+  );
 }
